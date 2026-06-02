2824a9079e94a3eea939d34c86f1656eb93ed763
Blaubart <Blaubart@gmx.de>
2026-06-02 18:57:17 +0200

    bugfix wind in FreeVario driver at XCSoar

--- src/Device/Driver/FreeVario.cpp
+++ src/Device/Driver/FreeVario.cpp
@@ -314,33 +314,35 @@ FreeVarioDevice::OnCalculatedUpdate(const MoreData &basic,
 
  NullOperationEnvironment env;
  char nmeaOutbuffer[80];
-/*
-Wie wird der Wind aktuell in XCSOar verwendet??
-
-
- if (!basic.external_instantaneous_wind_available.IsValid() && calculated.wind.IsNonZero() && basic.track_available){
-    const Angle relWindDirection = (calculated.wind.bearing - Angle::HalfCircle() - basic.track).AsBearing();
-    snprintf(nmeaOutbuffer,sizeof(nmeaOutbuffer),"PFV,AWD,%f", relWindDirection.Degrees());
-    PortWriteNMEA(port, nmeaOutbuffer, env);
-    snprintf(nmeaOutbuffer,sizeof(nmeaOutbuffer),"PFV,AWS,%f", calculated.wind.norm);
-    PortWriteNMEA(port, nmeaOutbuffer, env);
- }
  
- if (basic.external_wind_available.IsValid() && basic.attitude.heading_available){
-    const Angle relWindDirection = (basic.external_wind.bearing - Angle::HalfCircle() - basic.attitude.heading).AsBearing();     
-    snprintf(nmeaOutbuffer,sizeof(nmeaOutbuffer),"PFV,AWD,%f", relWindDirection.Degrees());
-    PortWriteNMEA(port, nmeaOutbuffer, env);
-    snprintf(nmeaOutbuffer,sizeof(nmeaOutbuffer),"PFV,AWS,%f", basic.external_wind.norm);
-    PortWriteNMEA(port, nmeaOutbuffer, env);
- }
+  if (!basic.external_wind_available.IsValid() &&
+      calculated.wind_available.IsValid() &&
+      calculated.wind.IsNonZero() &&
+      basic.track_available) {
+    const Angle relWindDirection =
+        (calculated.wind.bearing - Angle::HalfCircle() - basic.track).AsBearing();
 
- if (basic.external_instantaneous_wind_available.IsValid() && basic.attitude.heading_available){
-    const Angle relWindDirection = (basic.external_instantaneous_wind.bearing - Angle::HalfCircle() - basic.attitude.heading).AsBearing();     
-    snprintf(nmeaOutbuffer,sizeof(nmeaOutbuffer),"PFV,CWD,%f", relWindDirection.Degrees());
+    snprintf(nmeaOutbuffer, sizeof(nmeaOutbuffer), "PFV,AWD,%f", relWindDirection.Degrees());
     PortWriteNMEA(port, nmeaOutbuffer, env);
-    snprintf(nmeaOutbuffer,sizeof(nmeaOutbuffer),"PFV,CWS,%f", basic.external_instantaneous_wind.norm);
+    snprintf(nmeaOutbuffer, sizeof(nmeaOutbuffer), "PFV,AWS,%f", calculated.wind.norm);
     PortWriteNMEA(port, nmeaOutbuffer, env);
- }*/
+  }
+
+  if (basic.external_wind_available.IsValid() &&
+      basic.attitude.heading_available) {
+    const Angle relWindDirection =
+        (basic.external_wind.bearing - Angle::HalfCircle() -
+         basic.attitude.heading).AsBearing();
+
+    snprintf(nmeaOutbuffer, sizeof(nmeaOutbuffer), "PFV,AWD,%f", relWindDirection.Degrees());
+    PortWriteNMEA(port, nmeaOutbuffer, env);
+    snprintf(nmeaOutbuffer, sizeof(nmeaOutbuffer), "PFV,AWS,%f", basic.external_wind.norm);
+    PortWriteNMEA(port, nmeaOutbuffer, env);
+    snprintf(nmeaOutbuffer, sizeof(nmeaOutbuffer), "PFV,CWD,%f", relWindDirection.Degrees());
+    PortWriteNMEA(port, nmeaOutbuffer, env);
+    snprintf(nmeaOutbuffer, sizeof(nmeaOutbuffer), "PFV,CWS,%f", basic.external_wind.norm);
+    PortWriteNMEA(port, nmeaOutbuffer, env);
+  }
  
  // vario average last 30 secs
  snprintf(nmeaOutbuffer,sizeof(nmeaOutbuffer),"PFV,VAA,%f",calculated.average);
